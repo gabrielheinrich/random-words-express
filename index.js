2c783262ffbe3218da559ffb5ca382a0fa1a751a
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-09-03 14:10:50 +0200

    Snapshot for Franzi

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser")
+
+const db = {
+  words: ["javascript", "python", "php"]
+}
+const app = express();
+
+// Configuration
+// MIDDLEWARES
+app.use(logger("dev"))
+
+app.use(cors())
+
+// CONTROLLERS
+app.get("/hello", (request, response) => {
+  response.send({response: "Hello"})
+})
+
+app.get("/words/random", (request, response) => {
+  const randomIndex = Math.floor(Math.random() * db.words.length)
+  const word = db.words[randomIndex];
+  response.send({ word });
+})
+
+app.post("/words", (request, response) => {
+  console.log(request.body);
+})
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000")
+})
